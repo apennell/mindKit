24c9560439f0b57734f91edec7546ab636f3609c
Annie Pennell <annie@anniepennell.com>
2015-06-04 20:54:11 -0700

    backend server working with thoughtlog except emotion amounts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,29 +8,34 @@ angular.module('mindKit.controllers', [])
 
 .controller('ProgressCtrl', function($scope) {})
 
-.controller('NewLogCtrl', function($scope, ThoughtLog, $http) {
+.controller('NewLogCtrl', function($scope, ThoughtLog, $http, $state) {
   // get all thought logs
   $scope.thoughtLogs = ThoughtLog.query();
 
   // form data for creating new thought log with ng-model
   $scope.thoughtLog = {situation: "", emotion1: "", emotion2: "", emotion3: "", emotion4: "", emotion5: "", emotion1AmountA: "", emotion2AmountA: "", emotion3AmountA: "", emotion4AmountA: "", emotion5AmountA: "", thought: "", altThought: ""};
   $scope.createLog = function() {
-    console.log($scope.thoughtLog);
+    //console.log($scope.thoughtLog);
+    
+
+    console.log("I clicked createlog");
     $http.post('http://localhost:3000/logs', $scope.thoughtLog).
-      success(function(data, status, config) {
-        console.log(data);
+      success(function(data, status, headers, config) {
+        console.log("show that shit");
+        $state.go('showLog');
       }).
       error(function(data, status, config) {
-        console.log(data);
+        console.log("I got to error ", data);
       });
   }
+  
 })
 
 .controller('ShowLogCtrl', function($scope, $http) {
 
 
   $http.get('http://localhost:3000/logs').
-    success(function(data, status, config) {
+    success(function(data, status, headers, config) {
       console.log(data);
       $scope.thoughtLogs = data;
     }).
@@ -38,5 +43,3 @@ angular.module('mindKit.controllers', [])
       console.log(data);
     });
 })
-
-
